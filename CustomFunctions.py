0f4e294918bede4f69aae615ea1f6cbecc2ad375
JonasNey <JonasNey@googlemail.com>
2019-07-01 23:01:33 +0200

    Added Second Excercise

--- CustomFunctions.py
+++ CustomFunctions.py
@@ -1,5 +1,12 @@
 from datetime import datetime as dt
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+import warnings
+from IPython.display import clear_output
+from statsmodels.tsa.arima_model import ARIMA
+from fbprophet import Prophet
+import pandas as pd
 
 def get_min(df, lower_bound):
     """
@@ -217,3 +224,136 @@ def time_stamp_to_day(time_stamp):
     time = time_stamp.split(" ")[0]
     return time
 
+
+def plot_time_series_pred(train_data, test_data, pred_data, title):
+    """
+    Plots a time series graph of training data followed by test and prediction data.
+    """
+
+    date_list = list(train_data.index) + list(test_data.index)
+    train_range = list(range(len(train_data)))
+    test_range = list(range(len(train_data), len(train_data) + len(test_data)))
+
+    plt.plot(train_range, train_data.values, 'b', label="Training data")
+    plt.plot(test_range, test_data.values, 'bo', label="Correct")
+    plt.plot([train_range[-1], test_range[0]], [train_data.values[-1], test_data.values], 'b:')
+    plt.plot(test_range, pred_data, 'yo', label="Prediction")
+    plt.plot([train_range[-1], test_range[0]], [train_data.values[-1], pred_data], 'y:')
+    x_index_list_ticks, x_label_list_ticks = generate_plt_ticks(6, date_list)
+    plt.xticks(fontsize=8, rotation=30)
+    plt.xticks(x_index_list_ticks, x_label_list_ticks)
+    plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter('%1.2f €'))
+    plt.legend()
+    plt.title(title)
+    plt.show()
+
+
+def plot_time_series_pred_prophet(train_data, test_data, pred_data, title):
+    """
+    Plots a time series graph of training data followed by test and prediction data.
+    """
+
+    date_list = list(train_data["ds"]) + list(test_data["ds"])
+    train_range = list(range(len(train_data)))
+    test_range = list(range(len(train_data), len(train_data) + len(test_data)))
+
+    plt.plot(train_range, train_data["y"].values, 'b', label="Training data")
+    plt.plot(test_range, test_data["y"].values, 'bo', label="Correct")
+    plt.plot([train_range[-1], test_range[0]], [train_data["y"].values[-1], test_data["y"].values], 'b:')
+    plt.plot(test_range, pred_data.tail(len(test_data))["yhat"], 'yo', label="Prediction")
+    plt.plot([train_range[-1], test_range[0]], [train_data["y"].values[-1], pred_data.tail(len(test_data))["yhat"]], 'y:')
+    x_index_list_ticks, x_label_list_ticks = generate_plt_ticks(6, date_list)
+    plt.xticks(fontsize=8, rotation=30)
+    plt.xticks(x_index_list_ticks, x_label_list_ticks)
+    plt.gca().yaxis.set_major_formatter(ticker.FormatStrFormatter('%1.2f €'))
+    plt.legend()
+    plt.title(title)
+    plt.show()
+
+def suspress_ARIMA_warnigs():
+    """
+    Suspresses specific warnings thrown by the ARIMA module.
+    """
+
+    warnings.filterwarnings("ignore", message="Inverting hessian failed, no bse or cov_params available")
+    warnings.filterwarnings("ignore", message="overflow encountered in exp")
+    warnings.filterwarnings("ignore", message="invalid value encountered in true_divide")
+    warnings.filterwarnings("ignore", message="Maximum Likelihood optimization failed to converge. Check mle_retvals")
+
+
+def suspress_prophet_warnigs():
+    """
+    Suspresses specific warnings thrown by the ARIMA module.
+    """
+
+    warnings.filterwarnings("ignore", message="fbprophet:Disabling yearly seasonality. Run prophet with yearly_seasonality=True to override this.")
+    warnings.filterwarnings("ignore", message="fbprophet:Disabling daily seasonality. Run prophet with daily_seasonality=True to override this.")
+
+
+def get_mad(gt, pred):
+    """
+    Calculates the Mean Average Deviation
+    """
+
+    gt_mean = gt.mean()
+    mad = np.sum(np.abs((pred - gt_mean)))/pred.shape[0]
+    return mad
+
+
+def get_mape(gt, pred):
+    """
+    Calculates the Mean Absolute Percentage Error.
+    """
+
+    mape = 100*np.sum(np.abs((gt - pred)/gt))/pred.shape[0]
+    return mape
+
+
+def update_progress(progress):
+    """
+    Set progress bar to value of progress
+    """
+
+    bar_length = 20
+    if isinstance(progress, int):
+        progress = float(progress)
+    if not isinstance(progress, float):
+        progress = 0
+    if progress < 0:
+        progress = 0
+    if progress >= 1:
+        progress = 1
+    block = int(round(bar_length * progress))
+
+    clear_output(wait = True)
+    text = "Progress: [{0}] {1:.1f}%".format( "#" * block + "-" * (bar_length - block), progress * 100)
+    print(text)
+
+def apply_moving_average(train_data, test_data):
+    """
+    Apply simple moving average model to train_data.
+    Make prediction of len of test_data
+    """
+
+    # Set parameters of ARIMA model:
+    # 0: Order Autoregressive model
+    # 0: degree of differencing
+    # 4: order of moving average model
+    # -> MA model only
+    mw_model = ARIMA(train_data.values, order=(0, 0, 4))
+    mw_model_fit = mw_model.fit(start_params=(0, 0, 0, 0, 0))
+    pred = mw_model_fit.predict(start=len(train_data), end=(len(train_data) + len(test_data) - 1))
+    return pred
+
+
+def apply_fb_prophet(train_data, max_test_data_size):
+    """
+    Apply facebook prophet model to train_data.
+    Make prediction of len of max_test_data_size
+    """
+
+    m = Prophet()
+    m.fit(train_data)
+    future_df = m.make_future_dataframe(periods=max_test_data_size)
+    pred = m.predict(future_df)
+    return pred
